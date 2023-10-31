385676923a8b61dbcb1c97c2a6cd353f30e8c53f
rolando campos <rolando198431@hotmail.com>
2023-10-30 18:12:06 -0600

    Prueba unitaria de transaccion

---
+++ SistemaInventario.UnitTest/TransaccionTest.cs
@@ -0,0 +1,122 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Sistema_Inventario.Context;
+using Sistema_Inventario.dtos;
+using Sistema_Inventario.Entities;
+using Sistema_Inventario.Mappings;
+using Sistema_Inventario.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace SistemaInventario.UnitTest
+{
+    public class TransaccionesTest
+    {
+
+            private readonly TransaccionRepository _TransaccionRepository;
+            public TransaccionesTest()
+            {
+                var options = new DbContextOptionsBuilder<ApplicationDbContext>()  
+                    .UseSqlServer("Data Source = DESKTOP-RS7J03N; Initial Catalog = Sistema_Inventario; Integrated Security = True; Trust Server Certificate = True")
+                    .Options;
+
+                var dbContext = new ApplicationDbContext(options);
+
+                var configuartion = new MapperConfiguration(cfg =>
+                {
+                    cfg.AddProfile<MappingProfiles>();
+
+                });
+
+                var mapper = configuartion.CreateMapper();
+                _TransaccionRepository = new TransaccionRepository(dbContext, mapper);
+            }
+
+            [Fact]
+            public async void TestCrear()
+            {
+                //Arrange (Preparar)
+
+                var objeto = new GuardarTransaccion();
+                objeto.IdProducto = 1;
+                objeto.Fechayhora = DateTime.Now;
+                objeto.TipoTransaccion = "Traslado";
+                objeto.Cantidad = 2;
+                objeto.IdUsuario = 1;
+
+
+                //Act ( Actuar)
+                int resultado = await _TransaccionRepository.Crear(objeto);
+                //Assert (Afirmar)
+
+                Assert.True(resultado == 1);
+
+            }
+            [Fact]
+            public async void TestObtener()
+            {
+                //Arrange(Prepara)
+
+                //Act (Actuar)
+                var transaccion = await _TransaccionRepository.Transaccion();
+
+                //Assert(Afirmar)
+                Assert.True(transaccion.Count > 0);
+            }
+
+            [Fact]
+            public async void TestObtenerPorId()
+            {
+                //Arrange(Prepara)
+                int id = 2002;
+
+                //Act (Actuar)
+                var transaccion = await _TransaccionRepository.Transaccion(id);
+
+                //Assert(Afirmar)
+                Assert.NotNull(transaccion);
+            }
+            [Fact]
+            public async void TestModificar()
+            {
+                //Arrange(Prepara)
+                int id = 2002;
+                var objeto = new TransaccionDTO();
+                // objeto.Fechayhora = ";
+                objeto.TipoTransaccion = "Compra";
+                objeto.Cantidad = 10;
+                objeto.IdUsuario = 1;
+
+                //Act (Actuar)
+                int resultado = await _TransaccionRepository.Modificar(id, objeto);
+
+                //Assert(Afirmar)
+                Assert.Equal(1, resultado);
+            }
+
+            [Fact]
+            public async void TestEliminar()
+            {
+                //Arrange(Prepara )
+                int id = 2002;
+
+                //Act (Actuar)
+
+                int resultado = await _TransaccionRepository.Eliminar(id);
+
+                //Assert(Afirmar)
+                Assert.Equal(1, resultado);
+
+                //subir archivo de transacciones
+            }
+        }
+
+
+}
+
+
